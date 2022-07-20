45f75d9e5df3ccfd950abfdb16716a6ae572d3e5
emdadul_islam <emdadulislam439@gmail.com>
2022-07-20 11:17:28 +0600

    initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# destini_app_flutter
+
+This project is a part of London App Brewery's Flutter Learning Course. This is the 2nd challenge project.
+
---
+++ android/app/src/main/kotlin/com/example/destiny_app/destini_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.destiny_app.destini_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
